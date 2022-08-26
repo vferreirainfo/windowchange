01c198553765f6faa54dbadbdcd9711502339404
Vitor Ferreira <vferreirainfo@gmail.com>
2022-08-26 16:04:19 +0100

    Initial Commit

---
+++ windowchange/windowchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  windowchangeApp.swift
+//  windowchange
+//
+//  Created by Vitor on 26/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct windowchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
